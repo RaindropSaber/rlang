d4e708d00822254070e09435a297c20edc203fde
RaindropSaber <787145045@qq.com>
2022-10-12 18:22:33 +0800

    add web editor

---
+++ packages/test/node/index.ts
@@ -0,0 +1,9 @@
+'use strict';
+import requireRlang from '../../engine/lib';
+import path from 'path';
+
+const graph = requireRlang(path.resolve(__dirname, '../test-rlang/index.rl'));
+
+graph.setApp({});
+
+graph.start();
